22f221443ee738d23e398d29577b903f7a74a76d
Kiran <kiranyadav267@gmail.com>
2025-02-09 13:48:15 +0530

    abstract factory enhancement

---
+++ coding_fundamentals/LLD/Design Patterns/Creational Patterns/Extra Abstract Factory Layer/VehicleAbstractFactory.cs
@@ -0,0 +1,26 @@
+﻿using All.Design.Patterns.LLD.Design_Patterns.Creational_Patterns.Abstract_Factory.Car_Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_fundamentals.LLD.Design_Patterns.Creational_Patterns
+{
+    public class VehicleAbstractFactory
+    {
+        internal static IVehicleFactory CreateFactory(string type)
+        {
+            switch (type)
+            {
+                case "Car":
+                    return new CarFactory();
+                case "Bike":
+                    return new BikeFactory();
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
